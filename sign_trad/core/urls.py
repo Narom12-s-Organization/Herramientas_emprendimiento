cbd66d63171a2fc06a4e7f1a1fcb3b6ec0c2f8c9
Dieguito <105881222+diegoext514@users.noreply.github.com>
2023-10-04 18:30:49 -0300

    Styles.css y pagina index base

--- sign_trad/core/urls.py
+++ sign_trad/core/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path ('', include('core.urls')),
+    path('', views.index, name='index'),
 ]
